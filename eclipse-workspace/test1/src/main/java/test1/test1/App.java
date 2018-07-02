fb226a2003779f034c4a95ebdef680e57e84331e
poornima.p <poornima.p@C4LAP105.prodapt.com>
2018-07-02 13:40:24 +0530

    commit

---
+++ eclipse-workspace/test1/src/main/java/test1/test1/App.java
@@ -0,0 +1,18 @@
+package test1.test1;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+/**
+ * Unit test for simple App.
+ */
+public class App {
+	
+	public static void main() throws Exception{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\poornima.p\\eclipse-workspace\\test1\\drivers\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("http://www.google.com");
+	}
+
+}
